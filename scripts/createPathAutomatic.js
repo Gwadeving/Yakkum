4feeda60c0a71b2bbddbaea4c7dbed1d01db36ae
Gwadeving <shittemoiidesuka@gmail.com>
2025-04-07 19:03:27 +0700

    test

--- scripts/createPathAutomatic.js
+++ scripts/createPathAutomatic.js
@@ -3,7 +3,7 @@ function CreateConnectPath(from, to, color, type, anchorStart, anchorEnd, z) {
     svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
     svg.style.position = "absolute";
     svg.style.pointerEvents = "none";
-    svg.style.zIndex = z ? z : "-1";
+    svg.style.zIndex = z ? z : "1";
 
     console.log(z)
 
